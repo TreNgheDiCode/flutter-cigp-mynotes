615d299655981a480b0a2feca4aa03580c154cb2
TreNgheDiCode <gabayan170@gmail.com>
2023-08-26 18:33:29 +0700

    Unit Testing our AuthService

---
+++ test/auth_test.dart
@@ -0,0 +1,143 @@
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Mock Authentication', () {
+    final provider = MockAuthProvider();
+    test('Không được khởi tạo trước khi bắt đầu', () {
+      expect(provider.isInitialized, false);
+    });
+
+    test('Không thể đăng xuất khi chưa khởi tạo', () {
+      expect(
+        provider.logOut(),
+        throwsA(const TypeMatcher<NotInitializedException>()),
+      );
+    });
+
+    test('Được khởi tạo', () async {
+      await provider.initialize();
+      expect(provider.isInitialized, true);
+    });
+
+    test('Người dùng null sau khi khởi tạo', () {
+      expect(provider.currentUser, null);
+    });
+
+    test(
+      'Nên được khởi tạo dưới 3 giây',
+      () async {
+        await provider.initialize();
+        expect(provider.isInitialized, true);
+      },
+      timeout: const Timeout(Duration(seconds: 3)),
+    );
+
+    test('Tạo người dùng có thể đăng nhập', () async {
+      final badEmailUser = provider.createUser(
+        email: 'foobar@gmail.com',
+        password: 'any',
+      );
+
+      expect(
+        badEmailUser,
+        throwsA(const TypeMatcher<UserNotFoundAuthException>()),
+      );
+
+      final badPasswordUser = provider.createUser(
+        email: 'someone@gmail.com',
+        password: 'foobar',
+      );
+
+      expect(
+        badPasswordUser,
+        throwsA(const TypeMatcher<WrongPasswordAuthException>()),
+      );
+
+      final user = await provider.createUser(
+        email: 'foo',
+        password: 'bar',
+      );
+
+      expect(provider.currentUser, user);
+
+      expect(user.isEmailVerified, false);
+    });
+
+    test('Người dùng đã đăng nhập có thể xác thực email', () {
+      provider.sendEmailVerification();
+      final user = provider.currentUser;
+      expect(user, isNotNull);
+      expect(user!.isEmailVerified, true);
+    });
+
+    test('Có thể đăng xuất và đăng nhập lại', () async {
+      await provider.logOut();
+      await provider.logIn(
+        email: 'email',
+        password: 'password',
+      );
+      final user = provider.currentUser;
+      expect(user, isNotNull);
+    });
+  });
+}
+
+class NotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  AuthUser? _user;
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!isInitialized) throw NotInitializedException();
+    await Future.delayed(const Duration(seconds: 2));
+    return logIn(email: email, password: password);
+  }
+
+  @override
+  AuthUser? get currentUser => _user;
+
+  @override
+  Future<void> initialize() async {
+    await Future.delayed(const Duration(seconds: 2));
+    _isInitialized = true;
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    if (!isInitialized) throw NotInitializedException();
+    if (email == 'foobar@gmail.com') throw UserNotFoundAuthException();
+    if (password == 'foobar') throw WrongPasswordAuthException();
+    const user = AuthUser(isEmailVerified: false);
+    _user = user;
+    return Future.value(user);
+  }
+
+  @override
+  Future<void> logOut() async {
+    if (!isInitialized) throw NotInitializedException();
+    if (_user == null) throw UserNotLoggedInException();
+    await Future.delayed(const Duration(seconds: 2));
+    _user = null;
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    if (!isInitialized) throw NotInitializedException();
+    final user = _user;
+    if (user == null) throw UserNotLoggedInException();
+    const newUser = AuthUser(isEmailVerified: true);
+    _user = newUser;
+  }
+}
