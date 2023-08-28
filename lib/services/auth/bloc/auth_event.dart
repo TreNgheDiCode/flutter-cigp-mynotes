19937f5336c3a870a914aa2ea8b4fe6d9055302f
TreNgheDiCode <gabayan170@gmail.com>
2023-08-28 14:32:16 +0700

    Converting our Auth Process to Bloc

---
+++ lib/services/auth/bloc/auth_event.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+abstract class AuthEvent {
+  const AuthEvent();
+}
+
+class AuthEventInitialize extends AuthEvent {
+  const AuthEventInitialize();
+}
+
+class AuthEventLoggedIn extends AuthEvent {
+  final String email;
+  final String password;
+
+  const AuthEventLoggedIn(
+    this.email,
+    this.password,
+  );
+}
+
+class AuthEventLoggedOut extends AuthEvent {
+  const AuthEventLoggedOut();
+}
